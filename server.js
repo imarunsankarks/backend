724843edb29325730b723887bd49651c4c07483b
Arun Sankar <imarunsankarks@gmail.com>
2024-04-21 20:43:28 +0530

    cors update

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(cors());
 
 app.use(function (req, res, next) {
     next();
