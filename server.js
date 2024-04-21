58242c386845c945c8e4ca198c84cc229ad76e70
Arun Sankar <imarunsankarks@gmail.com>
2024-04-21 20:22:46 +0530

    url updated

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors()); // Using CORS middleware
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.use(function (req, res, next) {
     next();
@@ -19,8 +21,8 @@ app.use(function (req, res, next) {
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log('connected to db & listening to port ', process.env.PORT);
+        app.listen(process.env.PORT, '0.0.0.0', () => {
+            console.log('connected to db & listening to ', 'https://backend-v7tv.onrender.com/');
         });
     })
     .catch((err) => console.error(err));
